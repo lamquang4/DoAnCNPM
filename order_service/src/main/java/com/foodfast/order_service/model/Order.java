874114d8a9ff994ab9b4c5565e3ae424db492d15
ThomasHuynh <hoangtuan060494@gmail.com>
2025-10-04 16:19:55 +0700

    Initial commit for order_service project

---
+++ order_service/src/main/java/com/foodfast/order_service/model/Order.java
@@ -0,0 +1,36 @@
+package com.foodfast.order_service.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public class Order {
+    @Id
+    private String id;
+
+    private String userId;
+    private BigDecimal total;
+
+    private List<Item> items;
+
+    private Integer status; // 0: processing, 1: delivery, 2: done
+    private LocalDateTime timestamp;
+
+    public void setCreatedAt(LocalDateTime now) {
+    }
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class Item {
+    private String idProduct;
+    private Integer quantity;
+    private BigDecimal price;
+}
